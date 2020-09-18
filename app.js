7e627ff62f9d78446a7cbc08f2160b8349743a09
Wanzila Ronny <wanzilro@gmail.com>
2020-09-18 10:49:28 +0300

    posting and updating data

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
-
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
-// get request is a shlash /
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
-  res.send('hellow ron');
+  res.send('Welcome to my Nodemon API!');
 });
 
 app.listen(port, () => {
-  console.log(`Running on port + ${port}`);
+  console.log(`Running on port ${port}`);
 });
---
+++ booksJson.js
@@ -0,0 +1,23 @@
+db.books.insert([
+    {
+        bookId: 'bo123',
+        title: 'War and Peace',
+        genre: 'Histor fict',
+        author: 'ron',
+        read: false
+    },
+    {
+        bookId: 'bo123',
+        title: 'Love and Peace',
+        genre: 'Histor fict',
+        author: 'ae',
+        read: false
+    },
+    {
+        bookId: 'bo125',
+        title: 'War2 and Peace',
+        genre: 'tech fict',
+        author: 'ron2',
+        read: false
+    }
+])
